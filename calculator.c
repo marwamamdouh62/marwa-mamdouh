92ef48d722570cffe65ea4f82958e39c5bf1785d
marwa mamdouh <mamdouhmarwa759@gmail.com>
2022-09-09 01:21:26 +0200

    simple calculator

---
+++ calculator.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int main()
+{
+    float x;
+    float y; 
+    printf("enter the first number: ");
+    scanf("%f",&x);
+    printf("enter the second number: ");
+    scanf("%f",&y);
+    printf("the sum is: %f",x+y);
+    printf("the subtraction is: %f",x-y);
+
+
+
+}
