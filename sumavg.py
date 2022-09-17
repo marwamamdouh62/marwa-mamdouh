48e737ca0e8480da568dbd9a91dfa5ef5ad0562a
marwa mamdouh <mamdouhmarwa759@gmail.com>
2022-09-17 21:01:45 +0200

    sumavg.py

---
+++ sumavg.py
@@ -0,0 +1,9 @@
+n1= input("enter first number: ")
+n2= input("enter second number: ")
+n3= input("enter third number: ")
+
+r1= float(n1) + float(n2) + float(n3)
+r2= (float(n1) + float(n2) + float(n3))/3
+
+print("sum: " + str(r1))
+print("average: " + str(r2))
